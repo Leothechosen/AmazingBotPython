fc9a8f52f41c37b456c9c552bd9357971352e744
Leothechosen <Leosmarty98@gmail.com>
2020-02-09 10:36:25 -0800

    moved functions to util.py

--- cogs/esports.py
+++ cogs/esports.py
@@ -4,6 +4,7 @@ import asyncio
 import os
 import re
 import math
+import utils
 from pytz import timezone
 from datetime import datetime, timedelta
 from discord.ext import commands
@@ -27,7 +28,7 @@ class eSports(commands.Cog):
 		ordinal_message = ""
 		teams_message = ""
 		records_message = ""
-		tournamentId = await getTournamentId(self, tournament)
+		tournamentId = await utils.getTournamentId(tournament)
 		if tournamentId == "Invalid League":
 			await ctx.send("League not supported. Supported Leagues are: LCS, LEC, LCK, LPL, OPL, CBLOL, TCL, LJL, and LCSA(cademy)")
 			return
@@ -40,7 +41,7 @@ class eSports(commands.Cog):
 				for x in range(len(rankings)):
 					for y in range(len(rankings[x]["teams"])):
 						ordinal = str(rankings[x]['ordinal'])
-						ordinal = await integerPrefix(self, ordinal)
+						ordinal = await utils.integerPrefix(ordinal)
 						ordinal_message += (ordinal + '\n')
 						teams_message += str(rankings[x]["teams"][y]["name"]) + '\n'
 						records_message += str(rankings[x]["teams"][y]["record"]["wins"]) + "-" + str(rankings[x]["teams"][y]["record"]["losses"]) + '\n'
@@ -62,8 +63,8 @@ class eSports(commands.Cog):
 		if league == None:
 			await ctx.send("Usage: `-esports schedule [league] (Optional: [team abbreviation])`. Supported leagues are: LCS, LEC, LCK, LPL, OPL, CBLOL, TCL, LJL, and LCSA(cademy)")
 			return
-		leaguename = await sanitizeinput(self, league)
-		leagueid = await getLeagueId(self, league)
+		leaguename = await utils.sanitizeinput(league)
+		leagueid = await utils.getLeagueId(league)
 		if leagueid == "Invalid League":
 			await ctx.send("League not supported. Supported Leagues are: LCS, LEC, LCK, LPL, OPL, CBLOL, TCL, LJL, and LCSA(cademy)")
 			return
@@ -82,19 +83,23 @@ class eSports(commands.Cog):
 							starttime = re.sub('[Z]', "", starttime)
 							starttime = datetime.strptime(starttime, "%Y-%m-%d %H:%M:%S")
 							if currenttime + timedelta(days=7) > starttime:
-								days, hours, minutes = await days_hours_minutes(starttime - currenttime)
-								remainingtime = await timeformatting(days, hours, minutes)
+								days, hours, minutes = await utils.days_hours_minutes(starttime - currenttime)
+								remainingtime = await utils.timeformatting(days, hours, minutes)
 								schedule_message += remainingtime
-								teams1_message += "**" + scheduled_matches[x]["match"]["teams"][0]["name"] + " (" + str(scheduled_matches[x]["match"]["teams"][0]["record"]["wins"]) + "-" + str(scheduled_matches[x]["match"]["teams"][0]["record"]["losses"]) + ")" + "**\n"
-								teams2_message += "**" + scheduled_matches[x]["match"]["teams"][1]["name"] + " (" + str(scheduled_matches[x]["match"]["teams"][1]["record"]["wins"]) + "-" + str(scheduled_matches[x]["match"]["teams"][1]["record"]["losses"]) + ")" + "**\n"
+								if leaguename == "LCSA":
+									teams1_message += "**" + scheduled_matches[x]["match"]["teams"][0]["name"][:-8] + " (" + str(scheduled_matches[x]["match"]["teams"][0]["record"]["wins"]) + "-" + str(scheduled_matches[x]["match"]["teams"][0]["record"]["losses"]) + ")" + "**\n"
+									teams2_message += "**" + scheduled_matches[x]["match"]["teams"][1]["name"][:-8] + " (" + str(scheduled_matches[x]["match"]["teams"][1]["record"]["wins"]) + "-" + str(scheduled_matches[x]["match"]["teams"][1]["record"]["losses"]) + ")" + "**\n"
+								else: 
+									teams1_message += "**" + scheduled_matches[x]["match"]["teams"][0]["name"] + " (" + str(scheduled_matches[x]["match"]["teams"][0]["record"]["wins"]) + "-" + str(scheduled_matches[x]["match"]["teams"][0]["record"]["losses"]) + ")" + "**\n"
+									teams2_message += "**" + scheduled_matches[x]["match"]["teams"][1]["name"] + " (" + str(scheduled_matches[x]["match"]["teams"][1]["record"]["wins"]) + "-" + str(scheduled_matches[x]["match"]["teams"][1]["record"]["losses"]) + ")" + "**\n"
 						else:
 							if next_4_matches != 4:
 								if scheduled_matches[x]["match"]["teams"][0]["code"] == team or scheduled_matches[x]["match"]["teams"][1]["code"] == team:
 									starttime = re.sub('[T]', " ", scheduled_matches[x]["startTime"])
 									starttime = re.sub('[Z]', "", starttime)
 									starttime = datetime.strptime(starttime, "%Y-%m-%d %H:%M:%S")
-									days, hours, minutes = await days_hours_minutes(starttime - currenttime)
-									remainingtime = await timeformatting(days, hours, minutes)
+									days, hours, minutes = await utils.days_hours_minutes(starttime - currenttime)
+									remainingtime = await utils.timeformatting(days, hours, minutes)
 									schedule_message += remainingtime
 									if scheduled_matches[x]["match"]["teams"][0]["code"] == team:
 										team_record = "(" + str(scheduled_matches[x]["match"]["teams"][0]["record"]["wins"]) + "-" + str(scheduled_matches[x]["match"]["teams"][0]["record"]["losses"]) + ")"
@@ -129,7 +134,7 @@ class eSports(commands.Cog):
 		if team == None:
 			await ctx.send("Usage: `-esports team [team]. Example: -esports team TSM`. If the team has spaces, replace the spaces with dashes. For example: Counter-Logic-Gaming")
 			return
-		team = await sanitizeinput(team)
+		team = await utils.sanitizeinput(team)
 		async with aiohttp.ClientSession() as session:
 			headers = {'x-api-key': '0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z'}
 			async with session.get("https://esports-api.lolesports.com/persisted/gw/getTeams?hl=en-US&id=" + team.lower(),headers=headers) as response:
@@ -148,139 +153,6 @@ class eSports(commands.Cog):
 				embed.add_field(name="Name", value=name_message, inline=True)
 				embed.set_thumbnail(url=team_response["data"]["teams"][0]["image"])
 		await ctx.send(embed=embed)
-		
-async def integerPrefix(self, ordinal):
-						switcher = {
-							"1":"1st",
-							"2":"2nd",
-							"3":"3rd",
-							"4":"4th",
-							"5":"5th",
-							"6":"6th",
-							"7":"7th",
-							"8":"8th",
-							"9":"9th",
-							"10":"10th",
-							"11":"11th",
-							"12":"12th",
-							"13":"13th",
-							"14":"14th",
-							"15":"15th",
-							"16":"16th"
-							}
-						return switcher.get(ordinal, "???")
-	
-async def getTournamentId(self, tournament):
-	tournament = tournament.upper()
-	switcher = {
-		"LCS":"103462439438682788",
-		"LEC":"103462459318635408",
-		"LCK":"103540363364808496",
-		"LPL":"103462420723438502",
-		"OPL":"103535401218775284",	
-		"CBLOL":"103478354329449186",
-		"TCL":"103495775740097550",
-		"LJL":"103540397353089204",
-		"LCSA":"103462454280724883"
-			}
-	return switcher.get(tournament, "Invalid League")
-	
-async def getLeagueId(self, league):
-	league = league.upper()
-	switcher = {
-		"EUM":"100695891328981122",
-		"TAL":"101097443346691685",
-		"LLA":"101382741235120470",
-		"WORLDS":"98767975604431411",
-		"ALL-STARS":"98767991295297326",
-		"LCS":"98767991299243165",
-		"LEC":"98767991302996019",
-		"LCK":"98767991310872058",
-		"LPL":"98767991314006698",
-		"MSI":"98767991325878492",
-		"OPL":"98767991331560952",
-		"CBLOL":"98767991332355509",
-		"TCL":"98767991343597634",
-		"NAC":"98767991349120232",
-		"LCSA":"99332500638116286",
-		"LJL":"98767991349978712"
-			}
-	return switcher.get(league, "Invalid League")
-	
-async def timeformatting(days, hours, minutes):
-	if days == "0":
-		if hours == "0":
-			if minutes == "0":
-				remainingtime = ("Starting soon!\n")
-			elif minutes == "1":
-				remainingtime = (minutes + " minute\n")
-			else:
-				remainingtime = (minutes + " minutes\n")
-		if hours == "1":
-			if minutes == "0":
-				remainingtime = (hours + " hour\n")
-			elif minutes == "1":
-				remainingtime = (hours + " hour, " + minutes + " minute\n")
-			else:
-				remainingtime = (hours + " hour, " + minutes + " minutes\n")
-		else:
-			if minutes == "0":
-				remainingtime = (hours + " hours\n")
-			elif minutes == "1":
-				remainingtime = (hours + " hours, " + minutes + " minute\n")
-			else:
-				remainingtime = (hours + " hours, " + minutes + " minutes\n")
-	elif days == "1":
-		if hours == "0":
-			if minutes == "0":
-				remainingtime = (days + " day\n")
-			elif minutes == "1":
-				remainingtime = (days + " day, " + minutes + " minute\n")
-			else:
-				remainingtime = (days + " day, " + minutes + " minutes\n")
-		if hours == "1":
-			if minutes == "0":
-				remainingtime = (days + " day, " + hours + " hour\n")
-			elif minutes == "1":
-				remainingtime = (days + " day, " + hours + " hour\n")
-			else:
-				remainingtime = (days + " day, " + hours + " hour\n")
-		else:
-			if minutes == "0":
-				remainingtime = (days + " day, " + hours + " hours\n")
-			elif minutes == "1":
-				remainingtime = (days + " day, " + hours + " hours\n")
-			else:
-				remainingtime = (days + " day, " + hours + " hours\n")
-	else:
-		if hours == "0":
-			if minutes == "0":
-				remainingtime = (days + " days\n")
-			elif minutes == "1":
-				remainingtime = (days + " days, " + minutes + " minute\n")
-			else:
-				remainingtime = (days + " days, " + minutes + " minutes\n")
-		if hours == "1":
-			if minutes == "0":
-				remainingtime = (days + " days, " + hours + " hour\n")
-			elif minutes == "1":
-				remainingtime = (days + " days, " + hours + " hour\n")
-			else:
-				remainingtime = (days + " days, " + hours + " hour\n")
-		else:
-			if minutes == "0":
-				remainingtime = (days + " days, " + hours + " hours\n")
-			elif minutes == "1":
-				remainingtime = (days + " days, " + hours + " hours\n")
-			else:
-				remainingtime = (days + " days, " + hours + " hours\n")
-	return remainingtime
-	
-async def sanitizeinput(self, inputs):
-	return re.sub(r'[^a-zA-Z0-9-]', "", inputs)
-	
-async def days_hours_minutes(td):
-	return str(td.days), str(td.seconds//3600), str((td.seconds//60)%60)
 	
 def setup(bot):
 	bot.add_cog(eSports(bot))
