e822f3ac4665ee6646f16e01958489282e313211
Leothechosen <Leosmarty98@gmail.com>
2020-02-27 17:59:37 -0800

    Added "AmazingBot" to logging fmt

--- AmazingBotPython.py
+++ AmazingBotPython.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(
     filename="logging.log",
     level=logging.INFO,
-    format="%(asctime)s;%(levelname)s;%(message)s",
+    format="%(asctime)s;%(name)s;%(levelname)s;%(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 
