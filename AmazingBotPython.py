64ff7d40bfa7482897cdbbe49a846ca4c96ee0b5
Leothechosen <Leosmarty98@gmail.com>
2020-08-06 20:31:00 -0700

    Moved @bot.events -> owner.py, should lower bot restarts

--- AmazingBotPython.py
+++ AmazingBotPython.py
@@ -3,9 +3,6 @@
 import logging
 import os
 import discord
-import database
-import utils
-import traceback
 from datetime import datetime
 from dotenv import load_dotenv
 from discord.ext import commands
@@ -34,90 +31,12 @@ async def get_prefix(bot, message):
 bot = commands.Bot(command_prefix=get_prefix, owner_id=122919363656286212)
 bot.uptimeStart = datetime.now()
 
-@bot.event
-async def on_ready():
-    logger.info(f"AmazingBot connected to Discord | Discord.py Version {discord.__version__}")
-
-@bot.event
-async def on_connect():
-    game = discord.Game("Created By Leo")
-    await bot.change_presence(activity=game)
-
-@bot.event
-async def on_command(ctx):
-    logger.info(f' Message {ctx.message.content} - User: {ctx.message.author}')
-    bot.startTimer = datetime.now()
-    await ctx.trigger_typing()
-    return
-
-@bot.event
-async def on_command_completion(ctx):
-    logger.info(f"Completed in {datetime.now() - bot.startTimer}")
-
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.errors.CommandNotFound):
-        return
-    if isinstance(error, commands.errors.MissingPermissions):
-        await ctx.send("You do not have the proper Permissions to use this command.")
-        return
-    if isinstance(error, commands.errors.MissingRequiredArgument):
-        await ctx.send(f"You are missing required parameters. Use `-help {ctx.invoked_with}` to check what is required.")
-        return
-    logger.error(f"Error in {ctx.command}\n{error}")
-    logger.error("".join(traceback.format_tb(error.original.__traceback__)))
-    owner = bot.get_user(bot.owner_id)
-    await owner.send(f'Error in {ctx.command}\n{error}')
-    await ctx.send("Sorry, but there has been an error on processing this command. A notification has been sent to Leo")
-
-@bot.event
-async def on_guild_join(guild):
-    logger.info(f'AmazingBot has joined "{guild.name}"" | Guild_ID: {guild.id} | Owner_ID: {guild.owner_id} | # of members: {len(guild.members)}')
-    await bot.get_user(bot.owner_id).send(f'AmazingBot has joined "{guild.name}" \nGuild_ID: {guild.id}\nOwner_ID: {guild.owner_id}\n# of members: {len(guild.members)}')
-    await database.writeGuildPrefix(guild.id, "-")
-
-@bot.event
-async def on_guild_remove(guild):
-    logger.info(f'AmazingBot was removed from "{guild.name}" | Guild_ID: {guild.id} | Owner_ID: {guild.owner_id}')
-    await bot.get_user(bot.owner_id).send(f'AmazingBot was removed from "{guild.name}"\nGuild_ID: {guild.id}\nOwner_ID: {guild.owner_id}')
-
-@bot.event
-async def on_raw_reaction_add(payload):
-    try:
-        if payload.message_id != 701341554287181884:
-            return
-        role_reaction = await utils.role_reaction_emojis(str(payload.emoji))
-        role_to_add = await utils.role_reaction_roles(str(role_reaction))
-        guild = bot.get_guild(payload.guild_id)
-        role = guild.get_role(role_to_add)
-        member = guild.get_member(payload.user_id)
-        await member.add_roles(role)
-    except:
-        logger.exception("")
-    return
-
-@bot.event
-async def on_raw_reaction_remove(payload):
-    try:
-        if payload.message_id != 701341554287181884:
-            return
-        role_reaction = await utils.role_reaction_emojis(str(payload.emoji))
-        role_to_remove = await utils.role_reaction_roles(str(role_reaction))
-        guild = bot.get_guild(payload.guild_id)
-        role = guild.get_role(role_to_remove)
-        member = guild.get_member(payload.user_id)
-        await member.remove_roles(role)
-    except:
-        logger.exception("")
-    return
-
 @bot.command(hidden=True)
 @commands.is_owner()
 async def load(ctx, extension):
     """Owner Only | Loads a cog"""
     bot.load_extension(f"cogs.{extension}")
     await ctx.send(f"{extension} loaded")
-
 
 @bot.command(hidden=True)
 @commands.is_owner()
@@ -126,7 +45,6 @@ async def unload(ctx, extension):
     bot.unload_extension(f"cogs.{extension}")
     await ctx.send(f"{extension} unloaded")
 
-
 @bot.command(hidden=True)
 @commands.is_owner()
 async def reload(ctx, extension):
@@ -134,14 +52,6 @@ async def reload(ctx, extension):
     bot.unload_extension(f"cogs.{extension}")
     bot.load_extension(f"cogs.{extension}")
     await ctx.send(f"{extension} reloaded")
-
-
-@bot.command()
-async def ping(ctx):
-    """Pong"""
-    await ctx.send(f"Pong ({round(bot.latency*1000)}ms)")
-    return
-
 
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
