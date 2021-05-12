e846e9bbc0b3493ab6ad6a37457b26cfd59148df
Leothechosen <Leosmarty98@gmail.com>
2021-05-11 18:51:27 -0700

    Added more announcements, started fetching Amazingx clips

--- cogs/twitch.py
+++ cogs/twitch.py
@@ -4,6 +4,7 @@ import asyncio
 import os
 import random
 import apirequests
+import database
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
 import logging
@@ -14,11 +15,22 @@ load_dotenv()
 twitchtoken = os.getenv("TWITCH_API_KEY")
 amazingserverid = int(os.getenv("AMAZING_SERVER_ID"))
 amazing = os.getenv("AMAZING_CHANNEL")
+
 announcements = [
     "Go, get in there!",
     "Tune in and find out why they call him the Anaconda",
     "Come watch and soon you'll be Amazing yourself",
     "Don't knock him before you try him",
+    "Lmao you call yourself an Amazing fan? I'm fucking loaded on channel points bro. I highlight every message because I'm just that jacked. Try talking in chat again when you're a true fan",
+    "I hope Maurice wins xD. I'm an Amazingx simp and he's just so hot!!! People get so swooned by the hair, and his muscles are so swole like when he stretches LOL! He's super good but also smarter than he looks, just like me xD",
+    "My mom just entered my room and asked me to stop watching garbage. I told her that League of Legends was a respectable esport with millions of player around the world. She responded: “I know. I was talking about Amazingx, he's trash” KEKW.",
+    "Amazingx is real. I don't care what you want to say. Amazingx isn't 'imaginary'. He posts real tweets, feels real emotions, makes real bad plays...All you trolls are MALDING because he doesn't reply to your twitch messages probably. Not me though.. I have full faith in him. That's why he replied to me a couple minutes ago when I talked to him...he literally replied? And said 'shut the fuck up??'",
+    "Amazingx is fantastic, just needs to work on communication, map awareness, laning, vision control, csing, poking, landing skillshots, objective control, early game, mid game, late game, ganking, and getting kills",
+    "Hey, Amazing! Big fan of the stream, I can't believe you're not playing professionally anymore..RIP. I guess that's what happens when you become a coach, lol. Anyways, I'm trying to learn to play Shaco. I just have a question about the skill build: Should I max Backstab like you got backstabbed by Nicothepico, Decieve like you decieved Misfits into thinking they were a playoffs team, or Hallucinate like you made Origin hallucinate about being able to win Worlds 2015?",
+    "I want to date Amazing so bad. He is so cute and I love him so much. He has a cute smile and a cute face. He’s a gamer like me and has a amazing personality. I would love to tell him every night “I love you” and rub his feet while he is streaming We would go out when he isn’t doing gamer stuff and have fun together. He is also really good at gaming so I would be able to get carried by my very own BF. God I want to date Amazing",
+    "Who is amazing? 🙄🤭🤔🤫😰 in math: my solution ➗😊 in history: my king 👑😣 in art: my canvas 🎨🥳 in science: my oxygen 💨😝 in geography: my world 🌎🤯",
+    "Last Saturday, I found Amazing wet and unconscious on a beach. I quickly asked a lifeguard to watch over him while I get help. The lifeguard walks away muttering that he 'doesnt watch washed up streamers'",
+    "Jungle too complicated. I'd like to just rightclick the minimap once and autoclear every camp. Also: recalling should be automated once I killed the crab, else the skill ceiling is too high to differentiate between good and bad players"
 ]
 
 class Twitch(commands.Cog):
@@ -26,6 +38,7 @@ class Twitch(commands.Cog):
         self.bot = bot
         self.streamlive = True
         self.amazingStream.start() # pylint: disable=no-member
+        self.amazingClips.start() # pylint: disable=no-member
 
     @tasks.loop(minutes=1.0)
     async def amazingStream(self):
@@ -43,6 +56,30 @@ class Twitch(commands.Cog):
             )
             self.streamlive = True
             logger.info("Amazing has gone online")
+
+    @amazingStream.before_loop
+    async def amazingStreamBefore(self):
+        await self.bot.wait_until_ready()
+
+    @tasks.loop(minutes=5.0)
+    async def amazingClips(self):
+        clips_channel = int(os.getenv("AMAZING_CLIPS_CHANNEL"))
+        clips_channel = self.bot.get_channel(id=clips_channel)
+        clipsrequest = await apirequests.clips(amazing)
+        if clipsrequest is None:
+            return
+        clips = clipsrequest["data"]
+        clips_in_database = await database.getCurrentClips()
+        for clip in clips:
+            if clip["id"] in clips_in_database:
+                pass
+            else:
+                await clips_channel.send(f"A new clip has been created by {clip['creator_name']}\n{clip['url']}")
+                await database.addClip(clip["id"])
+    
+    @amazingClips.before_loop
+    async def amazingClipsBefore(self):
+        await self.bot.wait_until_ready()
 
 def setup(bot):
     bot.add_cog(Twitch(bot))
