8bd940fc29bd6b2ca79de74074ba5ad8db401ecb
Leothechosen <Leosmarty98@gmail.com>
2021-05-11 18:23:38 -0700

    logging -> logger (oops), added twitch OAauth for clips

--- apirequests.py
+++ apirequests.py
@@ -3,6 +3,7 @@ import re
 import discord
 import aiohttp
 import os
+import pytz
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import logging
@@ -13,8 +14,11 @@ load_dotenv()
 leagueapikey = os.getenv("LEAGUE_API_KEY")
 runeterraapikey = os.getenv("RUNETERRA_API_KEY")
 twitchapikey = os.getenv("TWITCH_API_KEY")
+twitchapisecret = os.getenv("TWITCH_API_SECRET")
 esportsapikey = os.getenv("ESPORTS_API_KEY")
 githubapikey = os.getenv("GITHUB_API_KEY")
+token_expire_time = None
+twitch_oauth_token = None
 
 
 async def esports(ctx, endpoint, param, paramId):
@@ -26,7 +30,7 @@ async def esports(ctx, endpoint, param, paramId):
         ) as response:
             if response.status != 200:
                 await ctx.send("Riot API returned a " + response.status + " error.")
-                logging.warning(f'eSports API returned a {response.status} response code. Endpoint: {endpoint} | Parameter_Name: {param} | Parameter_Value: {paramId}')
+                logger.warning(f'eSports API returned a {response.status} response code. Endpoint: {endpoint} | Parameter_Name: {param} | Parameter_Value: {paramId}')
                 return
             response = await response.json()
             await session.close()
@@ -45,7 +49,7 @@ async def league(ctx, region, endpoint, param, paramId):
                 elif endpoint == "match":
                     return
                 await ctx.send("Riot API returned a " + str(response.status) + " error.")
-                logging.warning(f'League API returned a {response.status} response code. Endpoint: {endpoint} | Parameter_Name: {param} | Parameter_Value: {paramId}')
+                logger.warning(f'League API returned a {response.status} response code. Endpoint: {endpoint} | Parameter_Name: {param} | Parameter_Value: {paramId}')
                 return
             response = await response.json()
             await session.close()
@@ -59,7 +63,7 @@ async def tft(ctx, region, endpoint, param, paramId):
         ) as response:
             if response.status != 200:
                 await ctx.send("Riot API returned a " + response.status + " error.")
-                logging.warning(f'TFT API returned a {response.status} response code. Endpoint: {endpoint} | Parameter_Name: {param} | Parameter_Value: {paramId}')
+                logger.warning(f'TFT API returned a {response.status} response code. Endpoint: {endpoint} | Parameter_Name: {param} | Parameter_Value: {paramId}')
                 return
             response = await response.json()
             await session.close()
@@ -73,11 +77,24 @@ async def lor(ctx, region, endpoint, param, paramId):
         ) as response:
             if response.status != 200:
                 await ctx.send("Riot API returned a " + response.status + " error.")
-                logging.warning(f'LoR API returned a {response.status} response code. Endpoint: {endpoint} | Parameter_Name: {param} | Parameter_Value: {paramId}')
+                logger.warning(f'LoR API returned a {response.status} response code. Endpoint: {endpoint} | Parameter_Name: {param} | Parameter_Value: {paramId}')
                 return
             response = await response.json()
             await session.close()
     return response
+
+async def twitch_oauth():
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f"https://id.twitch.tv/oauth2/token?client_id={twitchapikey}&client_secret={twitchapisecret}&grant_type=client_credentials") as response:
+            oauthrequest = await response.json()
+            logger.info(oauthrequest)
+            global token_expire_time
+            token_expire_time = datetime.now() + timedelta(seconds=oauthrequest["expires_in"])
+            global twitch_oauth_token
+            twitch_oauth_token = oauthrequest["access_token"]
+            logger.info(f"Twitch OAUTH Token: {twitch_oauth_token} expires at {token_expire_time}")
+        session.close()
+    return
 
 async def twitch(user):
     headers = {"Accept": "application//vnd.twitchtv.v5+json", "Client-ID": twitchapikey}
@@ -86,10 +103,35 @@ async def twitch(user):
             "https://api.twitch.tv/kraken/streams/" + user + "?api_verson=5", headers=headers
         ) as response:
             if response.status != 200:
-                logging.warning(f'Twitch API returned a {response.status} response code.')
+                logger.warning(f'Twitch API returned a {response.status} response code.')
             twitchrequest = await response.json()
         await session.close()
     return twitchrequest
+
+async def clips(user):
+    # Twitch Get Clips will only return clips in order of views, which impedes with posting a new clip when it's generated
+    # Luckily, I can pass a started_at query to all clips that were made after that start date
+    # Set started_at query to a day before the query. 
+    # Then store the clip IDs in a database
+    # Compare all the clip IDs in the response JSON to what's in the database
+    # If a clip is not in the database, send it to the clips channel in Amazing's Discord, then add the clip to the ID. 
+    if twitch_oauth_token is None:
+        await twitch_oauth()
+    elif datetime.now() > token_expire_time:
+        await twitch_oauth()
+    headers = {"Authorization": f"Bearer {twitch_oauth_token}", "Client-ID": twitchapikey}
+    started_at = datetime.utcnow() - timedelta(days=1)
+    started_at = started_at.isoformat("T") + "Z"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            f"https://api.twitch.tv/helix/clips/?broadcaster_id={user}&started_at={started_at}", headers=headers
+        ) as response:
+            if response.status != 200:
+                logger.warning(f"Twitch API returned a {response.status} response code in Clips.")
+                return None
+            response = await response.json()
+        session.close()
+    return response
 
 async def github():
     headers = {
@@ -127,7 +169,7 @@ async def clash_user(ctx, region, summonerid):
         ) as response:
             if response.status != 200:
                 await ctx.send("Riot API returned a " + str(response.status) + " error.")
-                logging.warning(f'Clash User API returned a {response.status} response code. Region: {region} | Summoner_ID: {summonerid}')
+                logger.warning(f'Clash User API returned a {response.status} response code. Region: {region} | Summoner_ID: {summonerid}')
                 return
             response = await response.json()
             await session.close()
@@ -141,7 +183,7 @@ async def clash_team(ctx, region, teamid):
         ) as response:
             if response.status != 200:
                 await ctx.send("Riot API returned a " + str(response.status) + " error.")
-                logging.warning(f'Clash Team API returned a {response.status} response code. Region {region} | Team_ID: {teamid}')
+                logger.warning(f'Clash Team API returned a {response.status} response code. Region {region} | Team_ID: {teamid}')
                 return
             response = await response.json()
             await session.close()
